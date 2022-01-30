891015b525422d6911ffdb25fe5d2e97cd95a865
Tito-74 <kipkirui133@gmail.com>
2022-01-30 11:39:25 +0300

    adding contact and footer part

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  alert('Please wait');
+  $(".card-body").hide();
+})
+$(document).ready(function(){
+  $(".card").mouseover(function(){
+    $('.card').css({"font-size": "160%","opacity": "30%"});
+    $(".card-body").show();
+  })
+})
+$(document).ready(function(){
+  $(".card").mouseout(function(){
+
+    $(".card-body").hide();
+  })
 })
